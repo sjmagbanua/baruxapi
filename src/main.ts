7d041eda71b3ad45d0511990457287962c88768d
sjmagbanua <snickersjay10@gmail.com>
2025-08-14 13:11:34 +0800

    app set for query parser use extended

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('query parser', 'extended');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
