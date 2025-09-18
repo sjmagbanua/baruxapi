6c366eb4a255078635a993bfec907e87b4d734a5
sjmagbanua <snickersjay10@gmail.com>
2025-09-19 06:52:38 +0800

    added name inside updateuserdto &

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    // Example: override to explicitly mark a field optional (though already optional via PartialType)
+    @IsOptional()
+    name?: string;
+
+    // You can also add additional fields specific to update
+    // @IsOptional()
+    // someOtherField?: string;
+}
