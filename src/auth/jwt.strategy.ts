75b851caef0d536913c63a1ccce3681e77037be4
sjmagbanua <snickersjay10@gmail.com>
2025-09-19 06:56:11 +0800

    added jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { jwtConstants } from './constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private configService: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET') || 'secretKey',
+        });
+    }
+
+    async validate(payload: any) {
+        return { userId: payload.sub, email: payload.email };
+    }
+}
